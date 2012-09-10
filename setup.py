8a2b332bbc445776d2cfd84b873e0c2a33845cee
Eric Davis <ed@npri.org>
2012-09-09 21:03:01 -0700

    Add URL to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = "0.1",
     author = "Eric Davis",
     author_email = "ed@npri.org",
+    url = "https://github.com/edavis/pypi-munin",
     py_modules = ["pypi"],
     entry_points = {
         "console_scripts": [
